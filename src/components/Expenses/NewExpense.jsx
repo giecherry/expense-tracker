aacfb2a8ac621b2a58960a0ad0222454197ce9af
Angie C <a.cajusol98@gmail.com>
2024-12-29 23:56:31 +0100

    new expense- css

--- src/components/Expenses/NewExpense.jsx
+++ src/components/Expenses/NewExpense.jsx
@@ -32,26 +32,20 @@ const NewExpense = () => {
     };
 
     return (
-        <div className="container bg-neutral-beige rounded-3xl p-8 h-[50%]">
-            <div >
-                    <div>
-                        <input className="rounded-3xl p-2 px-16 " type="text" placeholder="Description" onChange={(e)=> setExpenseDescription(e.target.value)}/> 
-                    </div>
-                    <div>
-                        <input className="rounded-3xl p-2 px-16 " placeholder="Amount" type="number" onChange={(e) => setExpenseAmount(e.target.value) } />
-                    </div>
-                    <div>
-                        <select className="rounded-3xl p-2 px-16  appearance-none border focus:outline-none" value={expenseCategory}onChange={(e) => setExpenseCategory(e.target.value)}>
-                            <option value="" disabled>Select a category</option>
-                            <option value="Housing">Housing</option>
-                            <option value="Transportation">Transportation</option>
-                            <option value="Groceries">Groceries</option>
-                            <option value="Lifestyle">Lifestyle</option>
-                            <option value="Others">Others</option>
-                        </select>
-                    </div>
+        <div className="container flex flex-col justify-around bg-neutral-beige rounded-3xl p-8 h-[50%] gap-2">
+                <input className="rounded-3xl p-2 px-16 w-[80%] " type="text" placeholder="Description" onChange={(e)=> setExpenseDescription(e.target.value)}/> 
+                <input className="rounded-3xl p-2 px-16 w-[80%] " placeholder="Amount" type="number" onChange={(e) => setExpenseAmount(e.target.value) } />
+                <div>
+                    <select className="rounded-3xl p-2 px-16 w-[80%] border-transparent appearance-none border focus:outline-none" value={expenseCategory}onChange={(e) => setExpenseCategory(e.target.value)}>
+                        <option value="" disabled>Select a category</option>
+                        <option value="Housing">Housing</option>
+                        <option value="Transportation">Transportation</option>
+                        <option value="Groceries">Groceries</option>
+                        <option value="Lifestyle">Lifestyle</option>
+                        <option value="Others">Others</option>
+                    </select>
                 </div>
-                <button onClick={handleAddExpense}>Add</button>
+            <button className="rounded-3xl p-2 px-4 m-2 w-1/4 bg-muted-rose hover:bg-deep-rose shadow-soft-pink" onClick={handleAddExpense}>Add</button>
         </div>
     );
 }
